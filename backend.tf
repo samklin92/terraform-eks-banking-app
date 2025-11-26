5418331b2395304da952833ed609654697fb47cc
Rita <ugwuanyinnenna43@gmail.com>
2025-11-26 20:26:54 +0000

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "digitalwitchngbucketcloud1"
-    key    = "digitalwitchng/prodution/terraform.tfstate"
-    region = "us-west-1"
+    bucket = "penta-ops-storage"
+    key    = "tt-aws/production/terraform.tfstate"
+    region = "us-east-1"
   }
 }
