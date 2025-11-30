5c97ff2a010f56e55a01ecf247e3e30943da81d0
Rita <ugwuanyinnenna43@gmail.com>
2025-11-30 23:21:14 +0000

    tf validate

--- 01-provider.tf
+++ 01-provider.tf
@@ -3,24 +3,24 @@ provider "aws" {
 }
 
 
-# terraform {
-#   required_providers {
-#     namecheap = {
-#       source  = "namecheap/namecheap"
-#       version = "2.2.0"
-#     }
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "6.0.0-beta2"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    namecheap = {
+      source  = "namecheap/namecheap"
+      version = "2.2.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0-beta2"
+    }
+  }
+}
 
-# provider "namecheap" {
-#   user_name   = var.namecheap_username
-#   api_user    = var.namecheap_api_user
-#   api_key     = var.namecheap_api_key
-#   client_ip   = var.namecheap_client_ip
-#   use_sandbox = false
-# }
+provider "namecheap" {
+  user_name   = var.namecheap_username
+  api_user    = var.namecheap_api_user
+  api_key     = var.namecheap_api_key
+  client_ip   = var.namecheap_client_ip
+  use_sandbox = false
+}
 
