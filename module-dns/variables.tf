cef03f316685eac485e88fcebe621fd5d74093e2
Rita <ugwuanyinnenna43@gmail.com>
2025-12-01 15:48:30 +0000

    tf validate

--- module-dns/variables.tf
+++ module-dns/variables.tf
@@ -1,15 +1,13 @@
-# variable "environment" {}
-# variable "domain-name" {}
-# variable "nginx_ingress_lb_dns" {
-#   description = "DNS name of the NGINX Ingress Load Balancer"
-#   type        = string
+variable "environment" {
+  type = string
+}
 
-# }
-# variable "nginx_lb_ip" {
-#   description = "IP address of the NGINX Ingress Load Balancer"
-#   type        = string
-# }
-# variable "nginx_ingress_load_balancer_hostname" {
-#   description = "Hostname of the NGINX Ingress Load Balancer"
-#   type        = string
-# }
+variable "domain_name" {
+  type = string
+}
+
+# Optional until NGINX ingress Load Balancer exists
+variable "nginx_lb_ip" {
+  type    = string
+  default = null
+}
