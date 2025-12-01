7e80a96fb2c10e9c2cb6222ab10c40fe7dacc783
Rita <ugwuanyinnenna43@gmail.com>
2025-12-01 17:35:09 +0000

    tf validate

--- output.tf
+++ output.tf
@@ -1,18 +1,8 @@
-#########################################################
-# Outputs from the eks-deployment module
-#########################################################
-
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = aws_eks_cluster.eks.name
+output "eks_cluster_name" {
+  description = "EKS cluster name from the eks-deployment module"
+  value       = module.eks-deployment.cluster_name
 }
 
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = aws_eks_cluster.eks.endpoint
-}
-
-output "cluster_certificate_authority_data" {
-  description = "EKS cluster CA certificate"
-  value       = aws_eks_cluster.eks.certificate_authority[0].data
+output "eks_cluster_endpoint" {
+  value = module.eks-deployment.cluster_endpoint
 }
