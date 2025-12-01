d4e33c37075763f24cc408de14a5f69bdc5d935e
Rita <ugwuanyinnenna43@gmail.com>
2025-12-01 17:03:41 +0000

    tf validate

--- module-eks/output.tf
+++ module-eks/output.tf
@@ -1,32 +1,29 @@
-#################################
-# EKS Outputs
-#################################
+#############################################
+# DATA SOURCE – Find Nginx Ingress LoadBalancer
+#############################################
 
-# output "cluster_name" {
-#   description = "EKS cluster name"
-#   value       = aws_eks_cluster.eks.name
-# }
+data "aws_lb" "nginx_ingress" {
+  tags = {
+    "app.kubernetes.io/name"      = "ingress-nginx"
+    "app.kubernetes.io/component" = "controller"
+  }
+}
 
-# output "cluster_endpoint" {
-#   description = "EKS cluster API endpoint"
-#   value       = aws_eks_cluster.eks.endpoint
-# }
-
-#################################
-# NGINX Ingress / LB Outputs
-#################################
+#############################################
+# OUTPUTS – Ingress Load Balancer Details
+#############################################
 
 output "nginx_ingress_lb_dns" {
-  description = "DNS name of the NGINX ingress load balancer"
+  description = "DNS name of the Nginx ingress load balancer"
   value       = data.aws_lb.nginx_ingress.dns_name
 }
 
 output "nginx_lb_ip" {
-  description = "IP (DNS name if IPv4) of the NGINX load balancer"
+  description = "LB IP/DNS depending on ingress type"
   value       = data.aws_lb.nginx_ingress.ip_address_type == "ipv4" ? data.aws_lb.nginx_ingress.dns_name : ""
 }
 
 output "nginx_ingress_load_balancer_hostname" {
-  description = "Hostname of the NGINX ingress load balancer"
+  description = "Hostname of ingress load balancer"
   value       = data.aws_lb.nginx_ingress.dns_name
 }
