d053aaaf7c9c831361eeda97f935ec118a6192b4
Cloudcrackamara <Cloudcrackamara@gmail.com>
2025-12-01 16:47:15 -0600

    updated DNS

--- module-dns/namecheap-name-servers.tf
+++ module-dns/namecheap-name-servers.tf
@@ -1,7 +1,12 @@
-resource "namecheap_domain_records" "my-domain2-com" {
-  domain = var.domain-name
-  mode = "OVERWRITE" // Warning: this will remove all manually set records
-
-nameservers = aws_route53_zone.r53_zone.name_servers
-depends_on = [ aws_route53_zone.r53_zone ]
-}
+# --------------------------------------------------------
+# Namecheap DNS – DISABLED to avoid overwriting nameservers
+# Manage nameservers in Namecheap UI using the NS from
+# aws_route53_zone.r53_zone.name_servers
+# --------------------------------------------------------
+# resource "namecheap_domain_records" "my-domain2-com" {
+#   domain = var.domain-name
+#   mode   = "OVERWRITE" // Warning: this will remove all manually set records
+#
+#   nameservers = aws_route53_zone.r53_zone.name_servers
+#   depends_on  = [aws_route53_zone.r53_zone]
+# }
--- module-dns/r52-hosted-zone.tf
+++ module-dns/r52-hosted-zone.tf
@@ -1,32 +1,45 @@
+#########################################################
+# Route53 Hosted Zone
+#########################################################
+
 resource "aws_route53_zone" "r53_zone" {
   name          = var.domain-name
   comment       = "Managed by Terraform"
-  force_destroy = true
+  force_destroy = false
 
   tags = {
     Name        = "${var.environment}-hosted-zone"
     Environment = var.environment
   }
 }
-resource "aws_route53_record" "name" {
+
+#########################################################
+# CNAME Records – all point to Nginx Ingress LB hostname
+#########################################################
+
+# bank app frontend
+resource "aws_route53_record" "bank" {
   zone_id = aws_route53_zone.r53_zone.zone_id
-  name    = "bank.${var.domain-name}" # Use a subdomain for CNAME
+  name    = "bank.${var.domain-name}"        # bank.pentaops.online
   type    = "CNAME"
   ttl     = 300
-  records = [var.nginx_lb_ip]
+  records = [module.eks-deployment.nginx_ingress_load_balancer_hostname]
 }
 
-resource "aws_route53_record" "name1" {
+# bank app backend/api
+resource "aws_route53_record" "bankapi" {
   zone_id = aws_route53_zone.r53_zone.zone_id
-  name    = "bankapi.${var.domain-name}" # Use a subdomain for CNAME
+  name    = "bankapi.${var.domain-name}"     # bankapi.pentaops.online
   type    = "CNAME"
   ttl     = 300
-  records = [var.nginx_lb_ip]
+  records = [module.eks-deployment.nginx_ingress_load_balancer_hostname]
 }
-resource "aws_route53_record" "name2" {
+
+# ArgoCD
+resource "aws_route53_record" "argocd" {
   zone_id = aws_route53_zone.r53_zone.zone_id
-  name    = "argocd.${var.domain-name}" # Use a subdomain for CNAME
+  name    = "argocd.${var.domain-name}"      # argocd.pentaops.online
   type    = "CNAME"
   ttl     = 300
-  records = [var.nginx_lb_ip]
+  records = [module.eks-deployment.nginx_ingress_load_balancer_hostname]
 }
