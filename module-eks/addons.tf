3dc1260400da1507b33270df99017484a0556201
Rita <ugwuanyinnenna43@gmail.com>
2025-12-02 21:14:20 +0000

    route53 deployment

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -35,10 +35,10 @@
 # ############################################
 # # Discover NGINX Ingress Load Balancer
 # ############################################
-# data "aws_lb" "nginx_ingress" {
-#   depends_on = [helm_release.nginx_ingress]
+data "aws_lb" "nginx_ingress" {
+  depends_on = [helm_release.nginx_ingress]
 
-#   tags = {
-#     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
-#   }
-# }
+  tags = {
+    "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
+  }
+}
