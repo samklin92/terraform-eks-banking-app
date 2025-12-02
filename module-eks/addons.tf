d7a2d032d954508634ed1cf7a89a8b38a527a8f3
Rita <ugwuanyinnenna43@gmail.com>
2025-12-02 21:12:08 +0000

    route53 deployment

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,44 +1,44 @@
-provider "helm" {
-  kubernetes = {
-    host                   = aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
+# provider "helm" {
+#   kubernetes = {
+#     host                   = aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "kubernetes" {
+#   host                   = aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks.token
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.eks.name
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = aws_eks_cluster.eks.name
+# }
 
-resource "helm_release" "nginx_ingress" {
-  name             = "nginx-ingress"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.12.0"
-  namespace        = "ingress-nginx"
-  create_namespace = true
+# resource "helm_release" "nginx_ingress" {
+#   name             = "nginx-ingress"
+#   repository       = "https://kubernetes.github.io/ingress-nginx"
+#   chart            = "ingress-nginx"
+#   version          = "4.12.0"
+#   namespace        = "ingress-nginx"
+#   create_namespace = true
 
-  values = [file("${path.module}/nginx-ingress-values.yaml")]
+#   values = [file("${path.module}/nginx-ingress-values.yaml")]
 
-  depends_on = [
-    aws_eks_node_group.eks_node_group
-  ]
-}
+#   depends_on = [
+#     aws_eks_node_group.eks_node_group
+#   ]
+# }
 
 
-############################################
-# Discover NGINX Ingress Load Balancer
-############################################
-data "aws_lb" "nginx_ingress" {
-  depends_on = [helm_release.nginx_ingress]
+# ############################################
+# # Discover NGINX Ingress Load Balancer
+# ############################################
+# data "aws_lb" "nginx_ingress" {
+#   depends_on = [helm_release.nginx_ingress]
 
-  tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
-  }
-}
+#   tags = {
+#     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
+#   }
+# }
