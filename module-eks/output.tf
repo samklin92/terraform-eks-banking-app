0d288916392f9204ae278c8294f1ee19c6a5534a
Rita <ugwuanyinnenna43@gmail.com>
2025-12-02 19:59:23 +0000

    route53 deployment

--- module-eks/output.tf
+++ module-eks/output.tf
@@ -1,14 +1,14 @@
 output "nginx_ingress_load_balancer_hostname" {
-  description = "Hostname of the NGINX Ingress Load Balancer created by Helm"
+  description = "Hostname of the NGINX Ingress Load Balancer"
   value       = data.aws_lb.nginx_ingress.dns_name
 }
 
 output "nginx_lb_ip" {
-  description = "Load balancer IP (optional, often empty for AWS LBs)"
-  value       = data.aws_lb.nginx_ingress.ip_address_type == "ipv4" ? data.aws_lb.nginx_ingress.dns_name : ""
+  description = "Fallback LB hostname (AWS LB has no IP)"
+  value       = data.aws_lb.nginx_ingress.dns_name
 }
 
 output "nginx_ingress_lb_dns" {
-  description = "DNS name of the NGINX ingress LB"
+  description = "DNS name of the NGINX ingress load balancer"
   value       = data.aws_lb.nginx_ingress.dns_name
 }
