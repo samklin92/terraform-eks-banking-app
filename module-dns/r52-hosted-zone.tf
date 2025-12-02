63a57ccae375310879e99e426a47d30ce060b4e8
Rita <ugwuanyinnenna43@gmail.com>
2025-12-02 19:21:26 +0000

    route53 deployment

--- module-dns/r52-hosted-zone.tf
+++ module-dns/r52-hosted-zone.tf
@@ -5,7 +5,7 @@
 resource "aws_route53_zone" "r53_zone" {
   name          = var.domain_name
   comment       = "Managed by Terraform"
-  force_destroy = true
+  force_destroy = false
 
   tags = {
     Name        = "${var.environment}-hosted-zone"
@@ -14,29 +14,32 @@ resource "aws_route53_zone" "r53_zone" {
 }
 
 #########################################################
-# CNAME Records
+# CNAME Records – All point to the Nginx LB Hostname
 #########################################################
 
+# Bank App Frontend
 resource "aws_route53_record" "bank" {
   zone_id = aws_route53_zone.r53_zone.zone_id
   name    = "bank.${var.domain_name}"
   type    = "CNAME"
   ttl     = 300
-  records = [module.eks-deployment.nginx_ingress_load_balancer_hostname]
+  records = [var.nginx_lb_hostname]
 }
 
+# Bank App Backend / API
 resource "aws_route53_record" "bankapi" {
   zone_id = aws_route53_zone.r53_zone.zone_id
   name    = "bankapi.${var.domain_name}"
   type    = "CNAME"
   ttl     = 300
-  records = [module.eks-deployment.nginx_ingress_load_balancer_hostname]
+  records = [var.nginx_lb_hostname]
 }
 
+# ArgoCD
 resource "aws_route53_record" "argocd" {
   zone_id = aws_route53_zone.r53_zone.zone_id
   name    = "argocd.${var.domain_name}"
   type    = "CNAME"
   ttl     = 300
-  records = [module.eks-deployment.nginx_ingress_load_balancer_hostname]
+  records = [var.nginx_lb_hostname]
 }
