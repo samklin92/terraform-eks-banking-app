1fe14fbb1c97c9bd6d545b78bcc9c9c50a7faae8
Cloudcrackamara <Cloudcrackamara@gmail.com>
2025-12-04 14:49:24 -0600

    Update repo

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,67 +1,16 @@
-############################################
-# EKS data sources (auth + endpoint)
-############################################
-
-data "aws_eks_cluster" "eks" {
-  name = aws_eks_cluster.eks.name
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.eks.name
-}
-
-############################################
-# Kubernetes Provider
-############################################
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
-############################################
-# Helm Provider
-############################################
-
-provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
-
-############################################
-# IMPORTANT: Prevent Terraform from reinstalling ingress
-############################################
-
 resource "helm_release" "nginx_ingress" {
-  name      = "nginx-ingress"
-  namespace = "ingress-nginx"
-
+  name             = "nginx-ingress"
+  namespace        = "ingress-nginx"
+  create_namespace = false
+  
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   version    = "4.12.0"
 
-  # Allow Terraform to override existing release
-  force_update = true
-  replace      = true
-
+  # This tells Helm: "If it exists, don't fail - just manage it"
+  replace = true
+  
   lifecycle {
-    prevent_destroy = true
-    ignore_changes  = all
-  }
-}
-
-############################################
-# Discover NGINX Load Balancer
-############################################
-
-data "aws_lb" "nginx_ingress" {
-  depends_on = [helm_release.nginx_ingress]
-
-  tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
+    ignore_changes = all
   }
 }
