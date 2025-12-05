f6bd49b4d303adc4b73c4eda81be95fe157dadd8
Cloudcrackamara <Cloudcrackamara@gmail.com>
2025-12-05 08:30:15 -0600

    Namespace

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,64 +1,86 @@
-############################################
-# EKS data sources (auth + endpoint)
-############################################
-
-data "aws_eks_cluster" "eks" {
-  name = aws_eks_cluster.eks.name
+provider "helm" {
+    kubernetes = {
+        host                   = aws_eks_cluster.eks.endpoint
+        cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+        token                  = data.aws_eks_cluster_auth.eks.token
+        
+    }
 }
-
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.eks.name
-}
-
-############################################
-# Kubernetes Provider
-############################################
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  alias                  = "eks"
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
-############################################
-# Helm Provider
-############################################
-
-provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
+data "aws_eks_cluster_auth" "eks" {
+    name = aws_eks_cluster.eks.name
 }
-
-############################################
-# IMPORTANT: Prevent Terraform from reinstalling ingress
-############################################
-
 resource "helm_release" "nginx_ingress" {
-  name      = "nginx-ingress"
-  namespace = "ingress-nginx"
+    name       = "nginx-ingress"
+    repository = "https://kubernetes.github.io/ingress-nginx"
+    chart      = "ingress-nginx"
+    version    = "4.12.0"
+    namespace  = "ingress-nginx"
+    create_namespace = true
 
-  # Dummy values to satisfy Terraform
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "4.12.0"
+    values = [file("${path.module}/nginx-ingress-values.yaml")]
 
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = all
-  }
+    force_update    = true
+    replace         = true
+    cleanup_on_fail = true
+    depends_on = [ aws_eks_node_group.eks_node_group, null_resource.wait_for_eks ]
 }
-
-############################################
-# Discover NGINX Load Balancer
-############################################
 
 data "aws_lb" "nginx_ingress" {
-  depends_on = [helm_release.nginx_ingress]
-
   tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
+    "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
   }
-} 
+
+  depends_on = [helm_release.nginx_ingress]
+}
+
+resource "helm_release" "cert_manager" {
+    name       = "cert-manager"
+    repository = "https://charts.jetstack.io"
+    chart      = "cert-manager"
+    version    = "1.14.5"
+    namespace  = "cert-manager"
+    create_namespace = true
+
+    set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    }
+  ]
+
+  force_update    = true
+  replace         = true
+  cleanup_on_fail = true
+    depends_on = [ helm_release.nginx_ingress, null_resource.wait_for_eks ]
+}
+#==================================================
+
+resource "helm_release" "argocd" {
+    name             = "argocd"
+    repository       = "https://argoproj.github.io/argo-helm"
+    chart            = "argo-cd"
+    version          = "5.51.6"
+    namespace        = "argocd"
+    create_namespace = true
+    values = [file("${path.module}/argocd-values.yaml")]
+
+    force_update    = true
+    replace         = true
+    cleanup_on_fail = true
+    depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager, null_resource.wait_for_eks ]
+}
+
+resource "null_resource" "wait_for_eks" {
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_eks_node_group.eks_node_group
+  ]
+}
