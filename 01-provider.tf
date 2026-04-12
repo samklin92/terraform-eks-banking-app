a9272a72a9bd60f37e5ff4ca0a005b497d884d14
samklin92 <your-email@gmail.com>
2026-04-12 13:27:20 +0100

    Fix Helm and Kubernetes provider auth using aws_eks_cluster data source

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,26 +1,56 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
-    namecheap = {
-      source  = "namecheap/namecheap"
-      version = "2.2.0"
-    }
     aws = {
       source  = "hashicorp/aws"
-      version = "6.0.0-beta2"
+      version = "~> 5.80"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.10"
     }
   }
 }
 
-provider "namecheap" {
-  user_name   = var.namecheap_username
-  api_user    = var.namecheap_api_user
-  api_key     = var.namecheap_api_key
-  client_ip   = var.namecheap_client_ip
-  use_sandbox = false
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = "banking-app"
+      Environment = var.environment
+      ManagedBy   = "terraform"
+    }
+  }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks-deployment.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks-deployment.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
