72c06bc023f03fb2b1c6997d038b7a6d245162ff
samklin92 <your-email@gmail.com>
2026-04-12 11:01:03 +0100

    Update backend.tf with correct S3 bucket name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "penta-deployment-bucket"
-    key    = "pentaops/prod/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "banking-app-tfstate-109804294707"
+    key            = "banking-app/prod/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "banking-app-terraform-lock"
   }
 }
