46b21bbc2c30092badfd57069e81013bbb777b10
samklin92 <your-email@gmail.com>
2026-04-12 11:33:20 +0100

    Mark sensitive outputs in root output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,38 @@
-#############################################
-# Clean Outputs for EKS Module
-#############################################
-
 output "eks_cluster_name" {
-  description = "EKS cluster name from the eks-deployment module"
+  description = "EKS cluster name"
   value       = module.eks-deployment.cluster_name
 }
 
 output "eks_cluster_endpoint" {
-  description = "EKS cluster endpoint from the eks-deployment module"
+  description = "EKS cluster endpoint"
   value       = module.eks-deployment.cluster_endpoint
+  sensitive   = true
 }
 
 output "eks_cluster_certificate_authority" {
-  description = "EKS Cluster CA data"
+  description = "EKS cluster certificate authority"
   value       = module.eks-deployment.cluster_certificate_authority_data
+  sensitive   = true
 }
 
+output "nginx_lb_hostname" {
+  description = "Nginx ingress load balancer hostname"
+  value       = module.eks-deployment.nginx_ingress_load_balancer_hostname
+}
+
+output "route53_name_servers" {
+  description = "Route53 nameservers - paste into Namecheap custom DNS"
+  value       = module.route53-deployment.route53_name_servers
+}
+
+output "ecr_backend_url" {
+  description = "ECR backend repository URL"
+  value       = module.eks-deployment.ecr_backend_url
+  sensitive   = true
+}
+
+output "ecr_frontend_url" {
+  description = "ECR frontend repository URL"
+  value       = module.eks-deployment.ecr_frontend_url
+  sensitive   = true
+}
